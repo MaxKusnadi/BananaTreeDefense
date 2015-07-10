4a04f9612ce2bc6c678fd9b1373d7f9ace463fd3
ckhan6241 <ckhan6241@hotmail.com>
2015-07-10 22:51:52 +0800

    converted the previous data type to new one

--- test.js
+++ test.js
@@ -295,8 +295,9 @@ level0 = {
 }
 */
 levelData = {
-	1:{events:[{time:[0,1,2], type:['Cow','Cow','Cow'], position:[0,1,2], wait:5},
-		{time:[1,2,3], type:['Chicken','Chicken','Chicken'], position:[3,2,1]}],
+	1:{events:[{time:[1, 5, 8, 10, 12, 15, 15, 20, 15, 25, 30, 35, 35, 35, 40, 45], 
+							type:["Cow", "Cow", "Cow", "Cow", "Chicken", "Cow", "Cow", "Cow", "Cow", "Chicken", "Chicken", "Cow", "Cow", "Cow", "Cow", "Chicken"], 
+							position:[0, 1, 1, 2, 3, 1, 2, 3, 1,0, 2, 1, 2, 3, 0, 1]}],
 		level: 1,
 		deploy: ["Soldier", "Archer"]},
 	2:{}
